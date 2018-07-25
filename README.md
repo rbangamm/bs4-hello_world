3e985b7f6675021746b42ab103ff51af605b2e45
rbangamm <ravindu.angammana@gmail.com>
2018-07-24 21:35:32 -0400

    Add Metacritic web spider

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-"# bs4-hello_world"
-
-Simple script that uses Beautiful Soup 4 to extract and display data from the most recent IGN-reviewed games.
+# Gamerevv
+Project that uses Beautiful Soup 4 to extract and display data from the highest reviewed games from IGN, Metacritic, and GameSpot using the pandas DataFrame.
--- script.py
+++ script.py
@@ -3,31 +3,46 @@ import urllib.request
 import pandas as pd
 import re
 
-titles = []
-dates = []
-scores = []
-platforms = []
+def fetch_data(url, process_func):
+	titles = []
+	dates = []
+	scores = []
+	platforms = []
 
-url = "http://ca.ign.com/reviews/games?"
-f = urllib.request.urlopen(url)
+	request = urllib.request.Request(url, None, {'User-Agent' : 'Mozilla'})
 
-text = f.read()
+	f = urllib.request.urlopen(request)
 
-soup = BeautifulSoup(text, 'html.parser')
+	text = f.read()
 
+	soup = BeautifulSoup(text, 'html.parser')
 
-tags = soup.find_all('div', class_='clear itemList-item')
+	process_func(soup, titles, dates, scores, platforms)
 
-for tag in tags:
-   t = tag.h3.a.text
-   match = re.search('\s*(.*)\s*', t)
-   titles.append(match.group(1))
-   dates.append(tag.find(lambda t: t.name == 'div' and t.get('class') == ['grid_3']).div.text)
-   scores.append(tag.find('span', class_="scoreBox-score").text)
-   platforms.append(tag.find('span', class_="item-platform").text)
+	dic = {'Title' : titles, 'Date' : dates, 'Platform' : platforms, 'Score' : scores}
+	df = pd.DataFrame(data=dic)
 
-dic = {'Title' : titles, 'Date' : dates, 'Platform' : platforms, 'Score' : scores}
+	return df
 
-df = pd.DataFrame(data=dic)
+def process_ign(soup, titles, dates, scores, platforms):
 
-print(df)
+	tags = soup.find_all('div', class_='clear itemList-item')
+
+	for tag in tags:
+	   t = tag.h3.a.text
+	   match = re.search('\s*(.*)\s*', t)
+	   titles.append(match.group(1))
+	   dates.append(tag.find(lambda t: t.name == 'div' and t.get('class') == ['grid_3']).div.text)
+	   scores.append(tag.find('span', class_="scoreBox-score").text)
+	   platforms.append(tag.find('span', class_="item-platform").text)
+
+def process_metacritic(soup, titles, dates, scores, platforms):
+
+	tags = soup.find_all('div', class_='product_row game', limit=25)
+
+	for tag in tags:
+		match = re.search('\s*(.+)\s*\((\w+)\)', tag.find('div', class_='product_item product_title').a.text)
+		titles.append(match.group(1))
+		platforms.append(match.group(2))
+		scores.append(tag.find('div', class_='product_item product_score').div.text)
+		dates.append(tag.find('div', class_='product_item product_date').text)
